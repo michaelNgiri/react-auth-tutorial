3fa361d42be137c008515fdd1f4ac43ab29fabc9
michaelNgiri <michaelngiri@gmail.com>
2019-12-12 20:15:50 +0100

    upload react auth tutorial project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react"
+import { BrowserRouter as Router, Link, Route } from "react-router-dom";
+import PrivateRoute from './PrivateRoute';
+import Home from "./Pages/Home";
+import Admin from "./Pages/Admin";
+import Login from './Pages/Login';
+import Signup from './Pages/Signup'
+import {AuthContext} from "./Context/Auth";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+
+function App(props) {
+  const [authTokens, setAuthTokens] = useState();
+
+  const setTokens = (data) => {
+    localStorage.setItem("tokens", JSON.stringify(data));
+    setAuthTokens(data);
+  }
+  return ( 
+  <AuthContext.Provider value={{ authTokens, setAuthTokens: setTokens }}>
+    <Router >
+     <div >
+        <ul > ... </ul>
+          <Route exact path="/" component={Home} />
+          <Route path="/login" component={Login} />
+          <Route path="/signup" component={Signup} />
+          <PrivateRoute path="/admin" component={Admin} />
+      </div> 
+      </Router> 
+    </AuthContext.Provider>
   );
 }
 
